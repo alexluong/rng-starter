d5832042a5fbcb9485a92f8a25d0a7f10267cd3f
alexluong <aluong@crimson.ua.edu>
2018-10-16 15:13:03 -0500

    Env

--- packages/client/webpack.config.js
+++ packages/client/webpack.config.js
@@ -1,42 +1,58 @@
 const path = require("path")
+const webpack = require("webpack")
+const dotenv = require("dotenv")
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader",
+module.exports = (_, argv) => {
+  // Prepare environment variables
+  const env = dotenv.config().parsed
+  env.NODE_ENV = argv.mode
+
+  const envKeys = Object.keys(env).reduce((prev, next) => {
+    prev[next] = JSON.stringify(env[next])
+    return prev
+  }, {})
+
+  return {
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
           },
-        ],
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
+        },
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: "html-loader",
+            },
+          ],
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+      ],
+    },
+    plugins: [
+      new webpack.DefinePlugin(envKeys),
+      new webpack.DefinePlugin({
+        "process.env": envKeys,
+      }),
+      new HtmlWebPackPlugin({
+        template: "./public/index.html",
+        filename: "./index.html",
+      }),
     ],
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: "./public/index.html",
-      filename: "./index.html",
-    }),
-  ],
-  resolve: {
-    modules: [path.resolve(__dirname, "./src"), "node_modules"],
-  },
-  devServer: {
-    port: 3000,
-    open: true,
-    historyApiFallback: true,
-  },
+    resolve: {
+      modules: [path.resolve(__dirname, "./src"), "node_modules"],
+    },
+    devServer: {
+      port: 3000,
+      historyApiFallback: true,
+    },
+  }
 }
